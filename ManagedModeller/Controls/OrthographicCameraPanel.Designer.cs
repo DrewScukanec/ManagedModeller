556093e279a2148d5ac03828bc67cc6d1d726c50
Drew Scukanec <andyscukanec@hotmail.com>
2016-02-29 00:05:57 +0000

    Added location, look at, and up controls to the ortho camera panel.

--- ManagedModeller/Controls/OrthographicCameraPanel.Designer.cs
+++ ManagedModeller/Controls/OrthographicCameraPanel.Designer.cs
@@ -23,33 +23,14 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.stringPanel1 = new ManagedModeller.Controls.StringPanel();
-            this.nearPanel = new ManagedModeller.Controls.FloatPanel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OrthographicCameraPanel));
             this.farPanel = new ManagedModeller.Controls.FloatPanel();
+            this.nearPanel = new ManagedModeller.Controls.FloatPanel();
+            this.stringPanel1 = new ManagedModeller.Controls.StringPanel();
+            this.locationPanel = new ManagedModeller.Controls.Vector3Panel();
+            this.lookAtPanel = new ManagedModeller.Controls.Vector3Panel();
+            this.upPanel = new ManagedModeller.Controls.Vector3Panel();
             this.SuspendLayout();
-            // 
-            // stringPanel1
-            // 
-            this.stringPanel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.stringPanel1.Label = "Name";
-            this.stringPanel1.Location = new System.Drawing.Point(0, 0);
-            this.stringPanel1.MinimumSize = new System.Drawing.Size(50, 20);
-            this.stringPanel1.Name = "stringPanel1";
-            this.stringPanel1.Size = new System.Drawing.Size(213, 20);
-            this.stringPanel1.TabIndex = 0;
-            this.stringPanel1.Value = "";
-            // 
-            // nearPanel
-            // 
-            this.nearPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.nearPanel.Label = "Near";
-            this.nearPanel.Location = new System.Drawing.Point(0, 20);
-            this.nearPanel.MaximumSize = new System.Drawing.Size(0, 20);
-            this.nearPanel.MinimumSize = new System.Drawing.Size(75, 20);
-            this.nearPanel.Name = "nearPanel";
-            this.nearPanel.Size = new System.Drawing.Size(213, 20);
-            this.nearPanel.TabIndex = 1;
-            this.nearPanel.Value = 0F;
             // 
             // farPanel
             // 
@@ -59,19 +40,83 @@
             this.farPanel.MaximumSize = new System.Drawing.Size(0, 20);
             this.farPanel.MinimumSize = new System.Drawing.Size(75, 20);
             this.farPanel.Name = "farPanel";
-            this.farPanel.Size = new System.Drawing.Size(213, 20);
+            this.farPanel.Size = new System.Drawing.Size(90, 20);
             this.farPanel.TabIndex = 2;
             this.farPanel.Value = 0F;
+            // 
+            // nearPanel
+            // 
+            this.nearPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.nearPanel.Label = "Near";
+            this.nearPanel.Location = new System.Drawing.Point(0, 20);
+            this.nearPanel.MaximumSize = new System.Drawing.Size(0, 20);
+            this.nearPanel.MinimumSize = new System.Drawing.Size(75, 20);
+            this.nearPanel.Name = "nearPanel";
+            this.nearPanel.Size = new System.Drawing.Size(90, 20);
+            this.nearPanel.TabIndex = 1;
+            this.nearPanel.Value = 0F;
+            // 
+            // stringPanel1
+            // 
+            this.stringPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.stringPanel1.Label = "Name";
+            this.stringPanel1.Location = new System.Drawing.Point(0, 0);
+            this.stringPanel1.MinimumSize = new System.Drawing.Size(50, 20);
+            this.stringPanel1.Name = "stringPanel1";
+            this.stringPanel1.Size = new System.Drawing.Size(90, 20);
+            this.stringPanel1.TabIndex = 0;
+            this.stringPanel1.Value = "";
+            // 
+            // locationPanel
+            // 
+            this.locationPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.locationPanel.Label = "Location";
+            this.locationPanel.Location = new System.Drawing.Point(0, 60);
+            this.locationPanel.MaximumSize = new System.Drawing.Size(0, 81);
+            this.locationPanel.MinimumSize = new System.Drawing.Size(79, 81);
+            this.locationPanel.Name = "locationPanel";
+            this.locationPanel.Size = new System.Drawing.Size(90, 81);
+            this.locationPanel.TabIndex = 3;
+            this.locationPanel.Value = ((OpenTK.Vector3d)(resources.GetObject("locationPanel.Value")));
+            // 
+            // lookAtPanel
+            // 
+            this.lookAtPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lookAtPanel.Label = "Look At";
+            this.lookAtPanel.Location = new System.Drawing.Point(0, 141);
+            this.lookAtPanel.MaximumSize = new System.Drawing.Size(0, 81);
+            this.lookAtPanel.MinimumSize = new System.Drawing.Size(62, 81);
+            this.lookAtPanel.Name = "lookAtPanel";
+            this.lookAtPanel.Size = new System.Drawing.Size(90, 81);
+            this.lookAtPanel.TabIndex = 4;
+            this.lookAtPanel.Value = ((OpenTK.Vector3d)(resources.GetObject("lookAtPanel.Value")));
+            // 
+            // upPanel
+            // 
+            this.upPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.upPanel.Label = "Up";
+            this.upPanel.Location = new System.Drawing.Point(0, 222);
+            this.upPanel.MaximumSize = new System.Drawing.Size(0, 81);
+            this.upPanel.MinimumSize = new System.Drawing.Size(62, 81);
+            this.upPanel.Name = "upPanel";
+            this.upPanel.Size = new System.Drawing.Size(90, 81);
+            this.upPanel.TabIndex = 5;
+            this.upPanel.Value = ((OpenTK.Vector3d)(resources.GetObject("upPanel.Value")));
             // 
             // OrthographicCameraPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.upPanel);
+            this.Controls.Add(this.lookAtPanel);
+            this.Controls.Add(this.locationPanel);
             this.Controls.Add(this.farPanel);
             this.Controls.Add(this.nearPanel);
             this.Controls.Add(this.stringPanel1);
+            this.MaximumSize = new System.Drawing.Size(0, 303);
+            this.MinimumSize = new System.Drawing.Size(79, 303);
             this.Name = "OrthographicCameraPanel";
-            this.Size = new System.Drawing.Size(213, 529);
+            this.Size = new System.Drawing.Size(90, 303);
             this.ResumeLayout(false);
 
         }
@@ -81,5 +126,8 @@
         private StringPanel stringPanel1;
         private FloatPanel nearPanel;
         private FloatPanel farPanel;
+        private Vector3Panel locationPanel;
+        private Vector3Panel lookAtPanel;
+        private Vector3Panel upPanel;
     }
 }
