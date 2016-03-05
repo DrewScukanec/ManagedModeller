1e5b60a954eb86367b5b7def68f392cd1cee46ee
Drew Scukanec <andyscukanec@hotmail.com>
2016-03-05 14:46:55 +0000

    Add an int panel.

---
+++ ManagedModeller/Controls/IntPanel.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ManagedModeller.Controls {
+    partial class IntPanel {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.label, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(58, 20);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label
+            // 
+            this.label.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(3, 3);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(14, 14);
+            this.label.TabIndex = 0;
+            this.label.Text = "L";
+            // 
+            // textBox
+            // 
+            this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox.Location = new System.Drawing.Point(23, 0);
+            this.textBox.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(35, 20);
+            this.textBox.TabIndex = 1;
+            // 
+            // IntPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximumSize = new System.Drawing.Size(0, 20);
+            this.MinimumSize = new System.Drawing.Size(50, 20);
+            this.Name = "IntPanel";
+            this.Size = new System.Drawing.Size(58, 20);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
---
+++ ManagedModeller/Controls/IntPanel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManagedModeller.Controls {
+    public partial class IntPanel : UserControl {
+
+        public delegate void ValueUpdated(int newValue);
+
+        private int lastGoodValue;
+        public event ValueUpdated valueUpdated;
+
+        public IntPanel() {
+            InitializeComponent();
+        }
+
+        [Category("User"), Description("Specifies the value of the textbox")]
+        public int Value {
+            get {
+                return Int32.Parse(textBox.Text);
+            }
+            set {
+                lastGoodValue = value;
+                UpdateTextBoxFromLastGoodValue();
+            }
+        }
+
+        [Category("User"), Description("Specifies the label on the textbox")]
+        public String Label {
+            get { return label.Text; }
+            set { label.Text = value; }
+        }
+
+        private void textBox_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+                TryUpdateValue();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                ParentForm.SelectNextControl(ActiveControl, true, true, true, true);
+            }
+        }
+
+        private void textBox_Enter(object sender, EventArgs e) {
+            textBox.SelectAll();
+        }
+
+        private void textBox_Leave(object sender, EventArgs e) {
+            TryUpdateValue();
+        }
+
+        private void TryUpdateValue() {
+            int result = 0;
+            bool success = Int32.TryParse(textBox.Text, out result);
+            if (success) {
+                lastGoodValue = result;
+                NotifyListeners();
+            } else {
+                UpdateTextBoxFromLastGoodValue();
+            }
+        }
+
+        private void NotifyListeners() {
+            if (valueUpdated != null) {
+                valueUpdated.Invoke(lastGoodValue);
+            }
+        }
+
+        private void UpdateTextBoxFromLastGoodValue() {
+            textBox.Text = lastGoodValue.ToString();
+        }
+    }
+}
