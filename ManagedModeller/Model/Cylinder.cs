02efae0cef03de4e84cf22cfca00d7c445a79062
Drew Scukanec <andyscukanec@hotmail.com>
2016-03-06 01:20:21 +0000

    Add normals to cylinder.

--- ManagedModeller/Model/Cylinder.cs
+++ ManagedModeller/Model/Cylinder.cs
@@ -17,35 +17,54 @@ namespace ManagedModeller.Model {
 
         #region Rendering
         public override void RenderInternal() {
-            DrawTop(1);
-            DrawTop(-1);
-            DrawSide(-1, 1);
+            DrawTop();
+            DrawBottom();
+            DrawSide();
         }
 
-        private void DrawTop(float height) {
+        private void DrawTop() {
             GL.Begin(PrimitiveType.TriangleFan);
 
-            GL.Vertex3(0, height, 0);
+            GL.Normal3(0f, 1f, 0f);
+            GL.Vertex3(0, 1, 0);
             for (int ctr = 0; ctr <= divisions * 4; ctr++) {
                 float radians = (float) Math.PI * 2 / (divisions * 4) * ctr;
                 float x = (float) Math.Cos(radians);
                 float z = (float) -Math.Sin(radians);
-                GL.Vertex3(x, height, z);
+                GL.Normal3(0f, 1f, 0f);
+                GL.Vertex3(x, 1, z);
             }
-            //GL.Vertex3(0, height, 0);
 
             GL.End();
         }
 
-        private void DrawSide(float bottom, float top) {
+        private void DrawBottom() {
+            GL.Begin(PrimitiveType.TriangleFan);
+
+            GL.Normal3(0f, -1f, 0f);
+            GL.Vertex3(0, -1, 0);
+            for (int ctr = divisions * 4; ctr >= 0; ctr--) {
+                float radians = (float)Math.PI * 2 / (divisions * 4) * ctr;
+                float x = (float)Math.Cos(radians);
+                float z = (float)-Math.Sin(radians);
+                GL.Normal3(0f, -1f, 0f);
+                GL.Vertex3(x, -1, z);
+            }
+
+            GL.End();
+        }
+
+        private void DrawSide() {
             GL.Begin(PrimitiveType.TriangleStrip);
 
             for (int ctr = 0; ctr <= divisions * 4; ctr++) {
                 float radians = (float)Math.PI * 2 / (divisions * 4) * ctr;
                 float x = (float)Math.Cos(radians);
                 float z = (float)-Math.Sin(radians);
-                GL.Vertex3(x, top, z);
-                GL.Vertex3(x, bottom, z);
+                GL.Normal3(x, 0, z);
+                GL.Vertex3(x, 1, z);
+                GL.Normal3(x, 0, z);
+                GL.Vertex3(x, -1, z);
             }
 
             GL.End();
